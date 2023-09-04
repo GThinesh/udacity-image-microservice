80caf7388775221c1b4968fa668c1c5ae183d468
Thinesh <thinesh@outlook.com>
2023-09-04 21:23:42 +0530

    deleted the file on on finish

--- server.js
+++ server.js
@@ -3,56 +3,55 @@ import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
 
 
+// Init the Express application
+const app = express();
 
-  // Init the Express application
-  const app = express();
+// Set the network port
+const port = process.env.PORT || 8082;
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-    /**************************************************************************** */
-
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-
-  app.get('/filteredimage', async (req,res)=>{
+app.get('/filteredimage', async (req, res) => {
     const imageUrl = req.query.image_url;
-    if(imageUrl){
-       const image= await filterImageFromURL(imageUrl);
-       res.sendFile(image);
-       deleteLocalFiles([image]);
+    if (imageUrl) {
+        console.log('Loading image ' + imageUrl);
+        try {
+            const image = await filterImageFromURL(imageUrl);
+            res.sendFile(image, err => {
+                if (err) {
+                    res.sendStatus(500);
+                }
+                console.log("Image send successfully");
+            });
+            res.on('finish',()=>{
+              deleteLocalFiles([image]);
+              console.log("Deleted image file " + image)
+            })
+        } catch (err) {
+            console.log("Error loading image");
+            res.status(422).send("Error occurred while reading image. Make sure the passed image is valid");
+        }
+    } else {
+        console.error("No image url is passed");
+        res.status(400).send("image_url should be a valid url");
     }
-    else{
-      console.error("No image url is passed");
-      res.status(400).send("image_url should be a valid url");
-    }
-  })
-  
+})
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+
+/**************************************************************************** */
+
+//! END @TODO1
+
+// Root Endpoint
+// Displays a simple message to the user
+app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+});
+
+
+// Start the Server
+app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+});
